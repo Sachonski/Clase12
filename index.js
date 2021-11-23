1ade2bbf1a693e90946fbfedd120cc8dc9ae71a3
Alejandro Telatnik <alejandrojtelatnik@gmail.com>
2021-11-22 22:16:52 -0300

    intento 5.2

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const path = require("path");
 const port = 3030;
 app.use(express.static("public"));
 
-app.listen(port, () => {
+app.listen(process.env.PORT || 4000, ()=>{
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/views/home.html"));
